335571d87a2d790ef2e690850655f0b37a37cbf7
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-06 22:05:53 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -48,8 +48,12 @@ def current_player
   end
 
 
-  def draw? #returns true for a draw and false for a win
+  def draw? #returns true for a draw and false for a win, or in progress game
     @board.full? && !won? ? true : false
+  #  game.board.cells = ["X", "O", "X", "O", "X", "X", "O", "X", "O"] true 
+  
+
+
   end
 
   def over?
