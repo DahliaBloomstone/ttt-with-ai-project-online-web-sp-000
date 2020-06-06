50a9ed99f7f37638c9be1949ea5e3ccc9c806fee
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-06 21:57:54 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -16,7 +16,7 @@ class Game
   def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
     @player_1 = player_1
     @player_2 = player_2
-    @board = board
+    @board = board #provides access to the board
     @board.display
   end
 
