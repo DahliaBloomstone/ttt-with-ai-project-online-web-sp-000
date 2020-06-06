290170e1149311d4e0c81caf460953d77f2b682b
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-06 22:03:23 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -31,7 +31,7 @@ def current_player
   #it 'returns false for a draw' do
   #game = Game.new
   #game.board.cells = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-    WIN_COMBINATIONS.each do |combination|   #[0,1,2] which is board range (0-8)
+    WIN_COMBINATIONS.each do |combination|  #iterate through each winning combination
         if @board.cells[combination[0]] == @board.cells[combination[1]] &&
           @board.cells[combination[1]] == @board.cells[combination[2]] &&
           @board.taken?(combination[0]+1)
